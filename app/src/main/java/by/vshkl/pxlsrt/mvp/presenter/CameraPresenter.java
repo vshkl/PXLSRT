2844f4933d4d7ce8992446396d886e0500ca1b70
Pavel Vashkel <jgugn1@gmail.com>
2017-01-10 16:29:18 +0300

    Update CameraPresenter. Update processPicture method

--- app/src/main/java/by/vshkl/pxlsrt/mvp/presenter/CameraPresenter.java
+++ app/src/main/java/by/vshkl/pxlsrt/mvp/presenter/CameraPresenter.java
@@ -1,22 +1,20 @@
 package by.vshkl.pxlsrt.mvp.presenter;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
 
 import java.io.FileOutputStream;
-import java.io.IOException;
 
+import by.vshkl.pxlsrt.core.utils.BitmapUtils;
 import by.vshkl.pxlsrt.mvp.view.CameraView;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 @InjectViewState
 public class CameraPresenter extends MvpPresenter<CameraView> {
 
-    private static final String TAG = CameraPresenter.class.getSimpleName();
-//    private static final int IMAGE_SIZE_PX = 1024;
     public static final int IMAGE_SIZE_PX = 1000;
 
     private Disposable disposable;
@@ -43,18 +41,15 @@ public class CameraPresenter extends MvpPresenter<CameraView> {
         getViewState().takePicture();
     }
 
-    //TODO: move this stuff to separate thread via RxJava2
-    public void processPicture(FileOutputStream fos, String filename, byte[] data) {
-        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-        bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getWidth());
-        bitmap = Bitmap.createScaledBitmap(bitmap, IMAGE_SIZE_PX, IMAGE_SIZE_PX, false);
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
-        try {
-            fos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        bitmap.recycle();
-        getViewState().sendPictureData(filename);
+    public void processPicture(FileOutputStream fos, String fname, byte[] data) {
+        disposable = BitmapUtils.saveTempBitmap(fos, fname, data)
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String string) throws Exception {
+                        getViewState().sendPictureData(string);
+                    }
+                });
     }
 }
