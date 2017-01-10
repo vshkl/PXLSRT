95933f33f385e50e80f7b2a05b57c8573fec6b64
Pavel Vashkel <jgugn1@gmail.com>
2017-01-10 16:28:37 +0300

    Create BitmapUtils for storing taked picture

---
+++ app/src/main/java/by/vshkl/pxlsrt/core/utils/BitmapUtils.java
@@ -0,0 +1,56 @@
+package by.vshkl.pxlsrt.core.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import by.vshkl.pxlsrt.mvp.presenter.CameraPresenter;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class BitmapUtils {
+
+    public static Observable<String> saveTempBitmap(final FileOutputStream fos, final String fname, final byte[] data) {
+        return Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(final ObservableEmitter<String> emitter) throws Exception {
+                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getWidth());
+                bitmap = Bitmap.createScaledBitmap(
+                        bitmap, CameraPresenter.IMAGE_SIZE_PX, CameraPresenter.IMAGE_SIZE_PX, false);
+                storeFile(fos, bitmap)
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(new Consumer<Boolean>() {
+                            @Override
+                            public void accept(Boolean aBoolean) throws Exception {
+                                if (aBoolean) {
+                                    emitter.onNext(fname);
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+    static Observable<Boolean> storeFile(final FileOutputStream fos, final Bitmap bitmap) {
+        return Observable.create(new ObservableOnSubscribe<Boolean>() {
+            @Override
+            public void subscribe(ObservableEmitter<Boolean> emitter) throws Exception {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+                bitmap.recycle();
+                try {
+                    fos.close();
+                    emitter.onNext(true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    emitter.onError(e);
+                }
+            }
+        });
+    }
+}
