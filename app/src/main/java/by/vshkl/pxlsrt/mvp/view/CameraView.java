3d66dcea2f9d9f0bfbffc4ad7f61788d9ec933bf
Pavel Vashkel <jgugn1@gmail.com>
2017-01-11 14:10:27 +0300

    Add runtime permissions. Change app version to 0.3.0

--- app/src/main/java/by/vshkl/pxlsrt/mvp/view/CameraView.java
+++ app/src/main/java/by/vshkl/pxlsrt/mvp/view/CameraView.java
@@ -4,6 +4,16 @@ import com.arellomobile.mvp.MvpView;
 
 public interface CameraView extends MvpView {
 
+    void checkPermissions();
+
+    void showPermissionMessage(int resId);
+
+    void showPermissionsRationale();
+
+    void requestPermissions();
+
+    void showAppSystemSettings();
+
     void toggleGrid();
 
     void toggleCamera();
--- app/src/main/java/by/vshkl/pxlsrt/ui/activity/CameraActivity.java
+++ app/src/main/java/by/vshkl/pxlsrt/ui/activity/CameraActivity.java
@@ -1,10 +1,21 @@
 package by.vshkl.pxlsrt.ui.activity;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
@@ -21,8 +32,10 @@ import by.vshkl.pxlsrt.ui.customview.GridView;
 
 public class CameraActivity extends MvpAppCompatActivity implements by.vshkl.pxlsrt.mvp.view.CameraView {
 
+    private static final int PERMISSION_REQUEST = 42;
     private static final String FNAME_PREFIX = "PXLSRT_";
 
+    @BindView(R.id.rl_root) RelativeLayout rlRoot;
     @BindView(R.id.cv_camera) CameraView cvCamera;
     @BindView(R.id.gv_grid_overlay) GridView gvGridOverlay;
     @BindView(R.id.iv_grid) ImageView ivGrid;
@@ -51,9 +64,22 @@ public class CameraActivity extends MvpAppCompatActivity implements by.vshkl.pxl
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                cvCamera.start();
+            } else {
+                cvCamera.stop();
+                presenter.showPermissionsMessage(R.string.permission_denied);
+            }
+        }
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
-        cvCamera.start();
+        presenter.checkPermissions();
     }
 
     @Override
@@ -91,6 +117,70 @@ public class CameraActivity extends MvpAppCompatActivity implements by.vshkl.pxl
     }
 
     //------------------------------------------------------------------------------------------------------------------
+
+    @Override
+    public void checkPermissions() {
+        if (checkSelfPermissions()) {
+            if (checkShouldShowRationale()) {
+                presenter.showPermissionsRationale();
+            } else {
+                presenter.requestPermissions();
+            }
+        } else {
+            cvCamera.start();
+        }
+    }
+
+    @Override
+    public void showPermissionMessage(int resId) {
+        final Snackbar snackbar = Snackbar.make(rlRoot, resId, Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction(R.string.button_settings, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.showAppSystemSettings();
+                snackbar.dismiss();
+            }
+        });
+        snackbar.show();
+    }
+
+    @Override
+    public void showPermissionsRationale() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.permission_rationale)
+                .setPositiveButton(R.string.button_allow, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        requestPermissions();
+                    }
+                })
+                .setNegativeButton(R.string.button_deny, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        presenter.showPermissionsMessage(R.string.permission_denied);
+                    }
+                })
+                .show();
+    }
+
+    @Override
+    public void requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE},
+                PERMISSION_REQUEST);
+    }
+
+    @Override
+    public void showAppSystemSettings() {
+        final Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_NO_HISTORY
+                | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        startActivity(intent);
+    }
 
     @Override
     public void toggleGrid() {
@@ -143,4 +233,16 @@ public class CameraActivity extends MvpAppCompatActivity implements by.vshkl.pxl
         startActivity(intent);
     }
 
+    //------------------------------------------------------------------------------------------------------------------
+
+    private boolean checkSelfPermissions() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean checkShouldShowRationale() {
+        return ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)
+                || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+    }
 }
