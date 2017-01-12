1ec2e503bbd03a18734c70dd859939d9c704bc8e
Pavel Vashkel <jgugn1@gmail.com>
2017-01-12 20:46:24 +0300

    Update TimeUtils to catch up with new speed!

--- app/src/main/java/by/vshkl/pxlsrt/core/utils/TimeUtils.java
+++ app/src/main/java/by/vshkl/pxlsrt/core/utils/TimeUtils.java
@@ -1,11 +1,10 @@
 package by.vshkl.pxlsrt.core.utils;
 
 import java.util.Locale;
-import java.util.concurrent.TimeUnit;
 
 public class TimeUtils {
 
     public static String getReadableTime(long time) {
-        return String.format(Locale.getDefault(), "%d sec", TimeUnit.MILLISECONDS.toSeconds(time));
+        return String.format(Locale.getDefault(), "%.3f sec", time / 1000.0);
     }
 }
