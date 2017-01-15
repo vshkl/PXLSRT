7889a5267447f61a171362b72310b31d090e6921
Pavel Vashkel <jgugn1@gmail.com>
2017-01-15 12:07:34 +0300

    Update TimeUtils class

--- app/src/main/java/by/vshkl/pxlsrt/core/utils/TimeUtils.java
+++ app/src/main/java/by/vshkl/pxlsrt/core/utils/TimeUtils.java
@@ -7,4 +7,8 @@ public class TimeUtils {
     public static String getReadableTime(long time) {
         return String.format(Locale.getDefault(), "%.3f sec", time / 1000.0);
     }
+
+    public static String getProcessingTimeToLog(long time) {
+        return String.format(Locale.getDefault(), "%.1f sec", time / 1000.0);
+    }
 }
